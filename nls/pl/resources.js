bfa396789e32f8f8ab3b88f57aabc67848104f26
Jarek <jaroslaw.bielec@pollub.edu.pl>
2017-08-07 07:23:17 +0200

    Initial commit

---
+++ nls/pl/resources.js
@@ -0,0 +1,53 @@
+﻿/** @license
+ | Copyright 2015 Esri
+ |
+ | Licensed under the Apache License, Version 2.0 (the "");
+ | you may not use this file except in compliance with the License.
+ | You may obtain a copy of the License at
+ |
+ |    http://www.apache.org/licenses/LICENSE-2.0
+ |
+ | Unless required by applicable law or agreed to in writing, software
+ | distributed under the License is distributed on an "" BASIS,
+ | WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ | See the License for the specific language governing permissions and
+ | limitations under the License.
+ */
+define({
+    messages: {
+        loadingApp: "Ładowanie aplikacji...",
+        loadingWebscene: "Ładowanie sceny 3D...",
+        noMoreSurveys: "Brak dostępnych ankiet. Dziękujemy za udział",
+        noSigninsAvailable: "Brak dostępnych loginów",
+        signinFetching: "Sprawdzanie dostępności możliwości logowania...",
+        surveySubmitted: "Ankieta została przesłana",
+        unableToFindSurvey: "Nie znaleziono ankiety.",
+        unableToFindSurveyInPopup: "Nie znaleziono okna popup.",
+        unableToStartApp: "Nie można wyświetlić ankiety. Dokończ konfigurację.",
+        unsupportedBrowser: "Ta przeglądarka nie jest obsługiwana<br>Minimalne wymagania do wyświetlenia modelu 3D znajdą Państwo <a href='http://doc.arcgis.com/pl/arcgis-online/reference/scene-viewer-requirements.htm'>TUTAJ</a>",
+        notFound: "Nie znaleziono"
+    },
+    prompts: {
+        clearBtn: "Wyczyść",
+        currentResponsesWillBeCleared: "Aktualna odpowiedź zostanie wyczyszczona. Czy chcesz kontynuować?",
+        finishBtn: "Zakończ",
+        goToResponses: "Przejdź do następnej lokalizacji",
+        nextBtn: "Następny",
+        nextResponseBtn: "Następna odpowiedź",
+        seeResponsesBtn: "Podgląd odpowiedzi",
+        signIn: "Rozpocznij ankietę",
+        submitBtn: "Wyślij",
+        turnOffResponsesBtn: "Wyłącz ankietę"
+    },
+    labels: {
+        closeBtn: "Zamknij",
+        exit: "Wyjdź",
+        guestName: "Gość",
+        multipleResponsesHere: "Ankieta",
+        signOut: "Wyloguj się"
+    },
+    tooltips: {
+        closeBtn: "Zakończono",
+        helpTip: "Więcej informacji"
+    }
+});
